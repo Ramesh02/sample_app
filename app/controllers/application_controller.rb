18af4ef2e6719f99069cb30257a0f5e042b0b985
ramesh02 <rameshchalla02@gmail.com>
2014-11-25 16:28:49 +0530

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  def show
-  	@application =application.find(params[:id])
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def hello
+  	render text: "Add hello"
 end
 end
